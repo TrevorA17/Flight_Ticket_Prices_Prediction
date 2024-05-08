24c82f77bb269cd11259e4e0c934eb6b1d00066e
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-08 09:49:49 +0300

    Measures of frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -23,3 +23,38 @@ head(flight_data)
 
 # View the dataset in a separate viewer window
 View(flight_data)
+
+#Measures of Frequency
+# Frequency of Journey Day
+journey_day_frequency <- table(flight_data$Journey_day)
+
+# Frequency of Airline
+airline_frequency <- table(flight_data$Airline)
+
+# Frequency of Class
+class_frequency <- table(flight_data$Class)
+
+# Frequency of Source
+source_frequency <- table(flight_data$Source)
+
+# Frequency of Departure
+departure_frequency <- table(flight_data$Departure)
+
+# Frequency of Total Stops
+total_stops_frequency <- table(flight_data$Total_stops)
+
+# Frequency of Arrival
+arrival_frequency <- table(flight_data$Arrival)
+
+# Frequency of Destination
+destination_frequency <- table(flight_data$Destination)
+
+# Display the frequency measures
+journey_day_frequency
+airline_frequency
+class_frequency
+source_frequency
+departure_frequency
+total_stops_frequency
+arrival_frequency
+destination_frequency
